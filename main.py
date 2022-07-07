0a347eff6247fde544f6fb341555c605fcffe608
Ryan Chiu <87596528+ryan314@users.noreply.github.com>
2022-07-06 23:23:32 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
-from flask import *
-import json, time
+from fastapi import FastAPI, Depends, Path, Query, HTTPException, status 
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/', methods=['GET'])
-def home_page():
-    data_set = {'Page': 'Home', 'Message': 'Successfully loaded the Home page', 'Timestamp': time.time()}
-    json_dump = json.dumps(data_set)
+class Note(BaseModel):
+    date: str
+    title: str 
+    notes: str
+    category: str
 
-    return json_dump 
+notes_library = {}
 
-@app.route('/user/', methods=['GET'])
-def request_page():
-    user_query = str(request.args.get('user')) # /user/?user=USER_NAME
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token') 
 
-    data_set = {'Page': 'Request', 'Message': f'Successfully got the request for {user_query}', 'Timestamp': time.time()}
-    json_dump = json.dumps(data_set)
+@app.post('/token')
+async def token(form_data: OAuth2PasswordRequestForm = Depends()): 
+    return {"NOTES": notes_library}
 
-    return json_dump 
 
-if __name__ == '__main__':
-    app.run(port=7777)
+@app.get('/gen_auth')
+async def gen_auth(token: str = Depends(oauth2_scheme)): 
+    return {'Authentication generation status': "Success"}
 
-# type "ngrok http 7777" in terminal to open up the interface
+
+@app.post("/create_item") 
+def create_note(*, note_id: int, note: Note):
+    if note_id in notes_library:
+        raise HTTPException(status_code = 400, detail = "Note ID already exists.")
+    
+    notes_library[note_id] = note
+    return notes_library[note_id]
+
+
+@app.get("/get_by_title")
+def get_by_title(*, title: str):
+    for i in notes_library:
+        if notes_library[i].title == title:
+            return notes_library[i]
+        else:
+            raise HTTPException(status_code = 401, detail = "Title does not exist.")
+
+
+@app.delete("/delete_note")
+def delete_note(*, title: str):
+    for i in notes_library:
+        if notes_library[i].title == title:
+            del notes_library[i]
+            return {"Success!": "Noted deleted!"}
+
+    raise HTTPException(status_code = 401, detail = "Title does not exist.")
